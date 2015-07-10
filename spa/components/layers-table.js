bc5a15676152e8a7f7fb3d4d7aaff96f4aaaaa65
Chad Wilcomb <chadwilcomb@gmail.com>
2015-07-10 17:38:19 -0400

    layers table link

--- spa/components/layers-table.js
+++ spa/components/layers-table.js
@@ -11,19 +11,19 @@ export default React.createClass({
         return (
             <table className='table-with-hover'>
               <thead>
-                <tr><th>Title</th><th>Description</th><th>Created By</th><th>Created On</th><th>Modified By</th><th>Modified On</th><th></th><th></th><th></th></tr>
+                <tr><th></th><th>Title</th><th>Description</th><th>Created By</th><th>Created On</th><th>Modified By</th><th>Modified On</th><th></th><th></th><th></th></tr>
               </thead>
               <tbody>
               {layers.map((layer) => {
                 return (
                   <tr key={layer._id}>
-                    <td>{layer.title}</td>
+                    <td><a href={layer.details_url} className='fa fa-globe' title='Details'></a></td>
+                    <td><a href={layer.details_url} title='Details'>{layer.title}</a></td>
                     <td>{layer.description}</td>
                     <td>{layer.creator.email}</td>
                     <td>{layer.created.toLocaleString()}</td>
                     <td>{layer.modifier.email}</td>
                     <td>{layer.modified.toLocaleString()}</td>
-                    <td><a href={layer.details_url} className='fa fa-globe' title='Details'></a></td>
                     <td><a href={layer.update_url} className='fa fa-pencil color green' title='Edit'></a></td>
                     <td><a href={layer.delete_url} className='fa fa-trash-o color red' title='Delete'></a></td>
                   </tr>
